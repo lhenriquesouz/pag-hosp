df2008846ec33a44b021579b023fcd75a680403b
Luis Henrique Souza <lhsouza_007@hotmail.com>
2021-02-09 14:18:04 -0300

    Alteração da index
    Alteração da index e style feita do serviço!

---
+++ index.js
@@ -0,0 +1,24 @@
+const spinnerLoading = document.querySelector('#loading')
+
+window.onscroll = function(){
+    scroll();
+};
+
+function scroll(){
+    let btn = document.getElementById("btnTop");
+    if(document.documentElement.scrollTop > 50){
+        btn.style.display = "block";
+    } else{
+        btn.style.display = "none";
+        }
+      
+}
+
+function backToTop(){
+    document.documentElement.scrollTop = 0;
+}
+
+setTimeout(() => {
+    spinnerLoading.remove()
+}, 1000)
+
