761553a0eabea828e326d29bdbb914c40ef08370
Luis Henrique Souza <lhsouza_007@hotmail.com>
2021-02-26 22:23:28 -0300

    Atualização da home-page

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-const spinnerLoading = document.querySelector('#loading')
-
 window.onscroll = function(){
     scroll();
-};
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+}
 
 function scroll(){
     let btn = document.getElementById("btnTop");
     if(document.documentElement.scrollTop > 50){
-        btn.style.display = "block";
-    } else{
-        btn.style.display = "none";
-        }
-      
-}
-
-function backToTop(){
+      btn.style.display = "block";
+    }else {
+      btn.style.display = "none";
+    }
+  }
+  function backToTop(){
     document.documentElement.scrollTop = 0;
-}
-
-setTimeout(() => {
-    spinnerLoading.remove()
-}, 1000)
-
+  }
